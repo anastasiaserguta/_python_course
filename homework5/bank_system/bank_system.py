accaec3e54b05b389b6094589deb64a2bd43da01
Anastasia Serguta <anastasia.serguta@gmail.com>
2024-05-26 22:33:22 +0300

    Some fixes and improvements.

--- homework5/bank_system/bank_system.py
+++ homework5/bank_system/bank_system.py
@@ -15,6 +15,10 @@ class Bank:
         else:
             print(f'Account with this login "{login}" exist, please, try again:(')
 
+    @staticmethod
+    def for_help():
+        pass
+
 
 
 class BankClient:
@@ -23,42 +27,44 @@ class BankClient:
         self._id = id
 
     @staticmethod
-    def check_client(login, id, client_base, attempts):
+    def check_client(login, id, client_base, attempts_enter_login, attempts_enter_id):
         try:
             if client_base[login][0] == id:
-                if name == 'nt':
-                    system('cls')
-                else:
-                    system('clear')
+                # if name == 'nt':
+                #     system('cls')
+                # else:
+                #     system('clear')
                 return f'Welcome, {login}!'
             else: 
-                print(f"Uncorrect ID. Try again or use command '3' (help)!")
-                id = str(input('Enter your ID (9 numbers): '))
-                if name == 'nt':
-                    system('cls')
+                print(f"Uncorrect ID. Try again or use command '#3' (help)!")
+                attempts_enter_id -= 1
+                if attempts_enter_id != 0:
+                    id = str(input('Enter your ID (9 numbers): '))
+                    return BankClient.check_client(login, id, client_base, attempts_enter_login, attempts_enter_id)
                 else:
-                    system('clear')
-                return BankClient.check_client(login, id, client_base, attempts)
+                    print('The limit of attempts has been exceeded! Try some later:(')
+                    for second in range(60, 0, -1):
+                        sleep(1.0)
+                        print(second, end='... ')
+                    print()
+                    attempts_enter_login = 5
+                    id = str(input('Enter your ID (9 numbers): '))
+                    return BankClient.check_client(login, id, client_base, attempts_enter_login, attempts_enter_id)
         except KeyError as log:
             print(f"Account with login {log} doesn't exist. Try again or use command '#3' (help)!") # КОМАНДА 3 НЕ СОЗДАНА!!!!
-            attempts -= 1
-            if attempts != 0:
+            attempts_enter_login -= 1
+            if attempts_enter_login != 0:
                 login = input('Enter your login: ')
-                return BankClient.check_client(login, id, client_base, attempts)
-            elif login == '#3':
-                return 'To get help, contact support (available also by phone - 999).'
+                return BankClient.check_client(login, id, client_base, attempts_enter_login, attempts_enter_id)
             else:
                 print('The limit of attempts has been exceeded! Try some later:(')
-                for i in range(60, 0, -1):
-                    print(i)
+                for second in range(60, 0, -1):
                     sleep(1.0)
-                    if name == 'nt':
-                        system('cls')
-                    else:
-                        system('clear')
-                attempts = 5
+                    print(second, end='... ')
+                print()
+                attempts_enter_login = 5
                 login = input('Enter your login: ')
-                return BankClient.check_client(login, id, client_base, attempts)
+                return BankClient.check_client(login, id, client_base, attempts_enter_login, attempts_enter_id)
 
 
 class BankAccount:
@@ -171,107 +177,107 @@ all_start_actions = {
     }
 
 def create_new_account():
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
+    # if name == 'nt':
+    #     system('cls')
+    # else:
+    #     system('clear')
     new_client = BankClient(input('Enter your first and last name. '), ''.join(map(str, sample(range(0, 10), 3)))) #ВЕРНУТЬ НОРМ ID!!!!
     Bank.add_bank_client_to_the_client_base(Bank.client_base, list(BankAccount.all_code_currency.keys()), login=new_client.name, id=new_client._id)
-    for second in range(30, 0, -1):
-        print(second)
-        sleep(1)
-        print(f'Your login: {new_client.name}. Your ID: {new_client._id}.')
-        print('Use them to login to your new bank account!')
-        print('Important! remember your ID, it is necessary to use the system.')
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
+    print(f'Your login: {new_client.name}. Your ID: {new_client._id}.')
+    print('Use them to login to your new bank account!')
+    print('Important! remember your ID, it is necessary to use the system.')
+    for second in range(5, 0, -1): # ИМЗЕНИТЬ КОЛИЧЕСТВО СЕКУНД -> 60 !!!!
+        sleep(1.0)
+        print(second, end='... ')
+    print()
     return login_to_account()
 
 def login_to_account():
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
+    # if name == 'nt':  # очистка терминала
+    #     system('cls')
+    # else:
+    #     system('clear')
     login = input('Enter your login: ')
     id = str(input('Enter your ID (9 numbers): '))
-    print(f'УДАЛИТЬ {login=}, {id=}') # нЕ ЗАБЫТЬ УДАЛИТЬ!!!
+    # print(f'{login=}, {id=}') # нЕ ЗАБЫТЬ УДАЛИТЬ!!!
     all_clients = Bank.client_base
-    total_attempts = 5
-    BankClient.check_client(login, id, all_clients, total_attempts)
-    sleep(10.0)
+    total_attempts_login = 5
+    total_attempts_id = 5
+    BankClient.check_client(login, id, all_clients, total_attempts_login, total_attempts_id)
     all_currency = BankAccount.all_code_currency
     answer = ''
     currency = 0
     created_accounts = []
     flag = False
     amount = 0
-    if name == 'nt':
-        system('cls')
-    else:
-        system('clear')
-    print(*BankAccount.all_actions_with_currency.items(), sep='\n')
-    print(f'What would you do, {login}?')
-    sleep(3.0)
-    while answer != 'stop':
+    # if name == 'nt':
+    #     system('cls')
+    # else:
+    #     system('clear')
+    # sleep(3.0)
+    while answer.lower() != 'stop':
+        print(*BankAccount.all_actions_with_currency.items(), sep='\n')
+        print(f'What would you do, {login}?')
         print('Enter "stop" for exit!')
-        answer = input('Enter your answer here, please: ')
+        answer = str(input('Enter your answer here, please: '))
+        if answer.lower() == 'stop':
+            print(f'Ok, {login}, see you!')
+            break
         print(*all_currency.items(), sep='\n')
-        currency = int(input('Select the currency (enter only code, please!): '))
+        try:
+            currency = int(input('Select the currency (enter only code, please!): '))
+        except ValueError as value:
+            print(f'{value} is not code. Maybe you wanted to do something else? Try again.')
         if answer in '+-':
-            try:
-                if len([k for k in range(1, 5) if type(all_clients[login][k][1]) != str]) == 0:
-                    print('You have no created account!')
-                    if str(input(f'Would you create account in {all_currency[currency]}? Enter "yes" or "no". ')).lower() == 'yes':
-                        BankAccount.create_bank_account(login, currency, all_clients, all_currency)
-                else:
-                    for j in range(1, 5):
-                        if type(all_clients[login][j][1]) != str:
-                            created_accounts.append(all_currency[all_clients[login][j][0]])
-                            if all_clients[login][j][0] == currency:
-                                flag = True
+            if len([k for k in range(1, 5) if type(all_clients[login][k][1]) != str]) == 0:
+                print('You have no created account!')
+                if str(input(f'Would you create account in {all_currency[currency]}? Enter "yes" or "no". ')).lower() == 'yes':
+                    BankAccount.create_bank_account(login, currency, all_clients, all_currency)
+            else:
+                for j in range(1, 5):
+                    if type(all_clients[login][j][1]) != str:
+                        created_accounts.append(all_currency[all_clients[login][j][0]])
+                        if all_clients[login][j][0] == currency:
+                            flag = True
 
-                    if flag:
-                        amount = Decimal(input(f'Enter amount in {all_currency[currency]}: '))
-                        BankAccount.change_bank_account(login, all_clients, answer, currency, amount, all_currency)
-                    else:
-                        print(f'Account in {all_currency[currency]} is not open. You have accounts: {''.join(created_accounts)}.')
-                        if input(f'Would you create account in {all_currency[currency]}? Enter "yes" or "no". ').lower() == 'yes':
-                            BankAccount.create_bank_account(login, currency, all_clients)
-            except ValueError as value:
-                print(f'{value} is not code. Maybe you wanted to do something else? Try again.')
+                if flag:
+                    amount = Decimal(input(f'Enter amount in {all_currency[currency]}: '))
+                    BankAccount.change_bank_account(login, all_clients, answer, currency, amount, all_currency)
+                else:
+                    print(f'Account in {all_currency[currency]} is not open. You have accounts: {''.join(created_accounts)}.')
+                    if input(f'Would you create account in {all_currency[currency]}? Enter "yes" or "no". ').lower() == 'yes':
+                        BankAccount.create_bank_account(login, currency, all_clients)
         elif answer in '?***':
-            pass
-        elif answer == 'stop':
-            break    
+            pass    
         else:
             print(f"{answer} isn't correct:( Try again...")
         
         print(f'What would you do next, {login}?')
-        print(*BankAccount.all_actions_with_currency.items(), sep='\n')
     
-    print(f'Ok, {login}, see you!')
     exit()
 
 def simple_bank_system(): # Функция, определяющая возможные действия пользователя.
     print(*all_start_actions.items(), sep='\n')
-    answer = int(input('What would you do? Please, enter command code. '))
-    if answer == 1:
-        create_new_account()
-    elif answer == 2:
-        login_to_account()
-    elif answer == 3:
-        exit()
-    else:
-        print(f'Answer {answer} is incorrect. Please, enter correct value!')
-        return simple_bank_system()
+    try:
+        answer = int(input('What would you do? Please, enter command code. '))
+        if answer == 1:
+            create_new_account()
+        elif answer == 2:
+            login_to_account()
+        elif answer == 3:
+            exit()
+        else:
+            print(f'Answer {answer} is incorrect. Please, enter correct value!')
+            return simple_bank_system()
+    except ValueError:
+        print('Use only numeric commands!')
+        simple_bank_system()
     
 
-if name == 'nt':
-    system('cls')
-else:
-    system('clear')
+# if name == 'nt':
+#     system('cls')
+# else:
+#     system('clear')
 
 simple_bank_system() # Запуск всей программы.
 
