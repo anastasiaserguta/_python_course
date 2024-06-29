eae3b8d42f7670226ce63e90950a3136d68d378d
Anastasia Serguta <anastasia.serguta@gmail.com>
2024-06-29 16:29:41 +0300

    Homework 7 added (data structures).

---
+++ homework7/queue.py
@@ -0,0 +1,90 @@
+class Node:
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self) -> str:
+        return f'{self.data}'
+    
+class QueueIterator: # Реализация протокола итерации.
+    def __init__(self, first_node):
+        self.current = first_node
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration
+        result = self.current.data
+        self.current = self.current.next
+
+        return result
+
+class Queue:
+    def __init__(self):
+        self._first_node = None
+        self._last_node = None
+        self._size = 0
+
+    def enqueue(self, item): # Добавление элемента в конец очереди.
+        new_data = Node(item)
+        if self._first_node is None:
+            self._first_node = new_data
+            self._last_node = self._first_node
+            self._size += 1
+        else:
+            self._last_node.next = new_data
+            self._last_node = new_data
+            self._size += 1
+
+    def dequeue(self): # Удаление и возврат элемента из начала очереди.
+        data = self._first_node.data
+        self._first_node = self._first_node.next
+        if self._first_node is None:
+            self._last_node is None
+        self._size -= 1
+        return data
+
+    def front(self): # Возврат элемента из начала очереди без его удаления.
+        return f'The first object - {self._first_node}.'
+
+    def is_empty(self): # Проверяет пустая ли очередь.
+        if self._first_node is None:
+            return 'Queue is empty.'
+        return 'Queue has data.'
+
+    def size(self): # Количество элементов в очереди.
+        if self._size > 0:
+            return f'Queue contains {self._size} objects.'
+        else:
+            return f'Queue has no objects.'
+
+    def display(self): # Вывод от начала до конца.
+        if self._first_node is None:
+            print('List is empty.')
+            return
+        current = self._first_node
+        while current:
+            print(current.data, end=' | ')
+            current = current.next
+        print()
+    
+    def __iter__(self): # Итерация по списку.
+        return QueueIterator(self._first_node)
+        
+if __name__ == '__main__':
+    queue = Queue()
+    print(queue.is_empty())
+    for i in range(5):
+        queue.enqueue(i)
+    first_item = queue.front()
+    print(first_item)
+    print(queue.size())
+    print(queue.is_empty())
+    queue.display()
+    print(queue.dequeue())
+    for elem in queue:
+        print(elem)
+    queue.display()
+    print(queue.size())
--- homework7/stack.py
+++ homework7/stack.py
@@ -5,43 +5,65 @@ class Node:
 
     def __str__(self) -> str:
         return f'{self.data}'
+    
+class StackIterator: # Реализация протокола итерации.
+    def __init__(self, top_node):
+        self.current = top_node
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current is None:
+            raise StopIteration
+        result = self.current.data
+        self.current = self.current.next
+
+        return result
 
 class Stack:
-    all_data = []
-
-    def __init__(self):
+    def __init__(self): # Инициализация стека.
         self._top_node = None
         self._size = 0
 
-    def push(self, item):
+    def push(self, item): # Добавление элемента в верхнюю часть стека.
         new_node = Node(data=item, next=self._top_node)
         self._top_node = new_node
         self._size += 1
-        Stack.all_data.append(self.__str__())
 
-    def pop(self):
+    def pop(self): # Удаления и возврат верхнего элемента стека.
         head = self._top_node
         self._top_node = self._top_node.next
         head.next = None
         self._size -= 1
         return head
 
-    def peek(self):
+    def peek(self): # Возврата верхнего элемента без его удаления.
         return f'The last object - {self._top_node}.'
 
-    def is_empty(self):
+    def is_empty(self): # Проверка пустоты стека.
         if self._top_node is None:
             return 'Stack is empty.'
         return 'Stack has data.'
 
-    def size(self):
-        return f'The stack contains {self._size} objects.'
+    def size(self): # Возврат размера стека.
+        if self._size > 0:
+            return f'Stack contains {self._size} objects.'
+        else:
+            return f'Stack has no objects.'
 
-    def display(self):
-        return Stack.all_data
+    def display(self): # Вывод стека от начала до конца.
+        if self._top_node is None:
+            print('List is empty.')
+            return
+        current = self._top_node
+        while current:
+            print(current.data, end=' | ')
+            current = current.next
+        print()
     
-    def __str__(self) -> str:
-        return f'{self._top_node}'
+    def __iter__(self):
+        return StackIterator(self._top_node)
 
 if __name__ == '__main__':
     stack = Stack()
@@ -50,10 +72,13 @@ if __name__ == '__main__':
         stack.push(i)
     last_item = stack.peek()
     print(last_item)
-    stack_size = stack.size()
-    print(stack_size)
+    for object in stack:
+        print(object)
+    stack.display()
+    print(stack.size())
     print(stack.is_empty())
     for _ in range(5):
         item = stack.pop()
         print(item)
-    print(*stack.display())
+    stack.display()
+    print(stack.size())
